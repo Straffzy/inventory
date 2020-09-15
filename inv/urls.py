9ae5dd441fec5f35215ffdabaf33cff88835acff
Jacqueline Arsenault <justcordelya@gmail.com>
2020-09-15 08:42:16 -0400

    basic url structure established

--- inv/urls.py
+++ inv/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('dash', views.dash, name='dashboard'),
+        path('search', views.search, name='search'),
+        path('reports', views.reports, name='reports'),
+        path('item', views.item),
+        path('box', views.box),
+        path('warehouse', views.warehouse),
+        path('consumable', views.consumable),
+        path('inventory', views.inventory, name='inventories'),
         ]
--- inv/views.py
+++ inv/views.py
@@ -4,5 +4,28 @@ from django.http import HttpResponse
 def index(request): 
     return HttpResponse("Hello, world. You're at the inventory index.")
 
+def dash(request):
+    return HttpResponse("Hello, world. You're at the Dashboard.")
 
-# Create your views here.
+def search(request):
+    return HttpResponse("Hello, world. You're at the Search Page.")
+
+def reports(request):
+    return HttpResponse("Hello, world. You're at the Reports Page.")
+
+def item(request):
+    return HttpResponse("Hello, world. You're at the Item Page.")
+
+def box(request):
+    return HttpResponse("Hello, world. You're at the Box Page.")
+
+def warehouse(request):
+    return HttpResponse("Hello, world. You're at the Warehouse Page.")
+
+def consumable(request):
+    return HttpResponse("Hello, world. You're at the Consumables Page.")
+
+def inventory(request):
+    return HttpResponse("Hello, world. You're at the Inventories Page.")
+
+
