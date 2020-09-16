4d2ccc69357eb0f6170078074075434a4475290d
Jacqueline Arsenault <justcordelya@gmail.com>
2020-09-16 12:18:03 -0400

    add an example credentials.py file for keeping secrets out of the git repo

---
+++ credentials-example.py
@@ -0,0 +1,25 @@
+# ========================================================= #
+#                Example credentials.py file                #
+#  Before using, make a copy and rename to "credentials.py" #
+#    ---> add credentials.py to your .gitignore file <---   #
+# ========================================================= #
+
+
+# Django Secret Key
+django = { 
+        secret_key : 'paste your secret key here'
+        }
+
+# In settings.py, do the following (remove comment #s)
+
+# This line goes before the SECRET_KEY line
+# or can be placed at/near the top of the file  
+
+# import credentials 
+
+# This line replaces the SECRET_KEY line AFTER you paste
+# the secret key into this file, above
+
+# SECRET_KEY = credentials.django['secret_key'] 
+
+# ============================================ #
