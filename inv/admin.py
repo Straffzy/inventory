4ac3416647b6077e73259b6124cac8cbe6fc4389
Jacqueline Arsenault <justcordelya@gmail.com>
2020-09-18 22:43:40 -0400

    Set number of extra forms to 1 For a lot of inlines, you don't need more than 1 form, and if you do need more than one, you can add one via javascript. Else you can save and continue editing to get another blank inline form.

--- inv/admin.py
+++ inv/admin.py
@@ -3,33 +3,41 @@ from . models import Warehouse, Staff, Boxes, Items, Items_in_boxes, Keywords, K
 
 class WarehouseInLine(admin.TabularInline):
     model = Boxes
+    extra = 1
 
 class WarehouseAdmin(admin.ModelAdmin):
     list_display = ('warehouse_name', 'warehouse_loc')
     inlines = [WarehouseInLine,]
+    extra = 1
 
 class StaffAdmin(admin.ModelAdmin):
     list_display = ('staff_name', 'staff_title')
+    extra = 1
 
 class BoxesAdmin(admin.ModelAdmin):
     list_display = ('box_id', 'warehouse', 'box_description')
-    
+    extra = 1
+
 class ItemsInLine(admin.TabularInline):
     model = Items_in_boxes
+    extra = 1
 
 class Keywords_Items(admin.TabularInline):
     model = Keywords_in_items
+    extra = 1
 
 class ItemsAdmin(admin.ModelAdmin):
     list_display = ('item_name', 'item_desc', 'item_value')
     inlines = [ItemsInLine, Keywords_Items]
+    extra = 1
 
 class Items_in_boxesAdmin(admin.ModelAdmin):
     list_display = ('box_id', 'item_id', 'date_from', 'date_to', 'moved_by_staff_id', 'reason')
+    extra = 1
 
 class Keywords_itemsAdmin(admin.ModelAdmin):
     list_display = ('item_id', 'keyword')
-
+    extra = 1
 
 admin.site.register(Warehouse, WarehouseAdmin)
 admin.site.register(Staff, StaffAdmin)
