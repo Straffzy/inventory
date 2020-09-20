b2f26d5baa3eae918c1779849c1ce54e045e2f57
Jacqueline Arsenault <justcordelya@gmail.com>
2020-09-20 18:47:29 -0400

    Reports stabilized Three different Item report views: Warehouses/boxes/items, Boxes/items, and Items all in tabular view. If a warehouse or box id is passed to the appropriate report url, the report will only show the data related to that warehouse or box. Each warehouse and box item name within a full report will link to that entity's corresponding report page by passing the entity's id to the relevant reports url. Links to the three item report types are available on the Reports page and the base.html navbar correctly points to the reports url.

--- inv/views.py
+++ inv/views.py
@@ -19,7 +19,7 @@ def search(request):
     return HttpResponse("Hello, world. You're at the Search Page.")
 
 def reports(request):
-    return render(request, inv/reports.html)
+    return render(request, 'inv/reports.html')
 
 def report_itm(request, item_sort=None):
     # item_sort 1 is alpha by item_name
@@ -29,8 +29,8 @@ def report_itm(request, item_sort=None):
     elif item_sort == 2:
          items = Items.objects.all().prefetch_related().annotate(box=F('itm_id__box_id__box_name')).annotate(wh=F('itm_id__box_id__warehouse__warehouse_name')).annotate(totval=Sum(F('item_value')*F('item_qty'), output_field=FloatField())).order_by('item_id')
     else:   
-         items = Items.objects.all().prefetch_related().annotate(box=F('itm_id__box_id__box_name')).annotate(wh=F('itm_id__box_id__warehouse__warehouse_nam    e')).annotate(totval=Sum(F('item_value')*F('item_qty'), output_field=FloatField()))
-    return render(request, inv/rpt_items.html, {'items' : items})
+         items = Items.objects.all().prefetch_related().annotate(box=F('itm_id__box_id__box_name')).annotate(wh=F('itm_id__box_id__warehouse__warehouse_name')).annotate(totval=Sum(F('item_value')*F('item_qty'), output_field=FloatField()))
+    return render(request, 'inv/rpt_items.html', {'items' : items})
     
 def report_box(request, box=None):
     if box:
@@ -38,7 +38,7 @@ def report_box(request, box=None):
     else: 
         boxes = Boxes.objects.all()
     items = Items.objects.all().prefetch_related().annotate(boxid=F('itm_id__box_id')).annotate(totval=Sum(F('item_value')*F('item_qty'), output_field=FloatField())).annotate(sort_name=Lower('item_name')).order_by('sort_name')
-    return render(request, inv/rpt_boxes.html, {'items' : items, 'boxes' : boxes})
+    return render(request, 'inv/rpt_boxes.html', {'items' : items, 'boxes' : boxes})
 
 def report_wh(request, whid=None):
     # warehouse report for a single warehouse
